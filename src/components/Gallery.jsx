31c97e3130d333fe06f7d06c3ea406b733e628a0
Asmit Kumar Roy <royasmit345@gmail.com>
2025-07-18 17:32:34 +0530

    Fixed the Gallery for Mobile view

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,22 +1,48 @@
 import React from "react";
 import "./Gallery.css";
 
+// Step 1: Import all your images directly into the component.
+// Make sure the path from this file to your images folder is correct.
+import img1 from "../assets/images/img1.png"; 
+import img2 from "../assets/images/img2.png";
+import img3 from "../assets/images/img3.png";
+import img4 from "../assets/images/img4.png";
+import img5 from "../assets/images/img5.png";
+import img6 from "../assets/images/img6.png";
+import img7 from "../assets/images/img7.png";
+
+// Step 2: Create an array of the imported images.
+const galleryImages = [
+  { id: "a", src: img1, area: "a" },
+  { id: "b", src: img2, area: "b" },
+  { id: "c", src: img3, area: "c" },
+  { id: "d", src: img4, area: "d" },
+  { id: "e", src: img5, area: "e" },
+  { id: "f", src: img6, area: "f" },
+  { id: "g", src: img7, area: "g" },
+];
+
 const Gallery = () => {
   return (
     <section className="photo-gallery">
-      <div class="photo-gallery">
-        <div class="gallery-grid">
-          <div class="item item1"></div>
-          <div class="item item2"></div>
-          <div class="item item3"></div>
-          <div class="item item4"></div>
-          <div class="item item5"></div>
-          <div class="item item6"></div>
-          <div class="item item7"></div>
-        </div>
+      <h1 className="other-Header">Glimpses of Our Journey</h1>
+      <div className="gallery-grid">
+        {/* Step 3: Map over the array to render each item */}
+        {galleryImages.map((image) => (
+          <div
+            key={image.id}
+            className="item"
+            style={{
+              // The grid-area is now applied directly for desktop view
+              gridArea: image.area,
+              // The background image is now applied safely via JS
+              backgroundImage: `url(${image.src})`,
+            }}
+          ></div>
+        ))}
       </div>
     </section>
   );
 };
 
-export default Gallery; 
+export default Gallery;
