4254e2b28c55fcbcdb4870ad9357fd25f05aee0c
Asmit Kumar Roy <royasmit345@gmail.com>
2025-08-07 01:49:26 +0530

    Updated upcoming event details in Banner component with new link and registration date

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import "./Banner.css";
 
 const Banner = () => {
   return (
     <div className="banner-wrapper">
       <div className="banner-text">
-        🚀 Upcoming Event: Idea Utsav 2025 | 💡 Register now and showcase your innovation! | 🎉 Don’t miss out!
+        🚀 Upcoming Event: <a href="https://reskilll.com/hack/troubleshoot">TroubleShoot Ideathon 2025</a> | 💡 Register now and showcase your innovation! | Last date to Register : 22 August 2025 | 🎉 Don&rsquo;t miss out!
       </div>
     </div>
   );
